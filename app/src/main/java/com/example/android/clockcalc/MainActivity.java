c23792e22437b24356c6988aa0c85b80553ce474
CannibalChoco <fudgeblimp@gmail.com>
2017-12-14 15:33:43 +0200

    Set up data binding

--- app/src/main/java/com/example/android/clockcalc/MainActivity.java
+++ app/src/main/java/com/example/android/clockcalc/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.android.clockcalc;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.clockcalc.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
 }
